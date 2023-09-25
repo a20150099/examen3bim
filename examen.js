26773021a4458c4786f098394064ab27a0096db7
a20150099 <131031942+a20150099@users.noreply.github.com>
2023-09-25 13:29:28 -0500

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,25 @@
+var nombre = prompt("Escribe tu nombre")
+var repuesta1 = prompt("1. ¿Cuánta agua hay en la Tierra?\nA.- Alrededor de un 41% de la superficie terrestre es agua.\nB.- Alrededor de un 51% de la superficie terrestre es agua\nC Alrededor de un 71% de la superficie terrestre es agua")
+var respuesta2 = prompt("2-¿Qué océano es el más grande?\nA.- El Pacífico\nB.- El Atlántico\nC.- El Índico")
+var respuesta3 = prompt("3. ¿Cuánto mide la parte más profunda de los Océanos?\nA.- 11 Km\n B.- 23 Km\nC.- 31 Km")
+
+var parseInt =(puntaje) = 0
+
+if(respuesta1 == "C"){
+    var puntaje = puntaje + 25
+}
+
+else(respuesta1)
+
+if(respuesta2 == "A"){
+    var puntaje = puntaje + 25}
+
+
+
+if(respuesta3 == "A"){
+    var puntaje = puntaje + 25}
+
+
+
+
+alert=("¡Saludos" + nombre)
